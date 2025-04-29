d3df6e29f9fb27cf7ea2941b455b0980f3a82edf
ꦄꦤ꧀ꦝꦿꦶ <59006481+ngadeg@users.noreply.github.com>
2025-04-29 15:34:15 +0700

    Update conf.cpp

--- conf.cpp
+++ conf.cpp
@@ -3,9 +3,5 @@ hPif : src/main.o src/fann_utils.o src/hashes.o src/config.o # added the config 
 
 config.o : src/config.cpp src/config.h
     g++ -c src/config.cpp
-
-.
-.
-.
 main.o: src/main.cpp src/config.h src/main.h src/hashes.h
     g++ -c src/main.cpp
